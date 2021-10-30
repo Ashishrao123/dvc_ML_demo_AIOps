3c79e9312cc5b170ad8454d3611753d8e75c664b
Ashish <ashishrao123@gmail.com>
2021-10-30 09:15:04 +0530

    stage_01_load_save added

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="src",
     version="0.0.1",
-    author="c17hawke",
+    author="Ashishrao123",
     description="A small package for dvc ml pipeline demo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/c17hawke/dvc-ML-demo-AIOps",
-    author_email="sunny.c17hawke@gmail.com",
+    url="https://github.com/Ashishrao123/dvc_ML_demo_AIOps",
+    author_email="ashishrao123@gmail.com",
     packages=["src"],
     python_requires=">=3.7",
     install_requires=[

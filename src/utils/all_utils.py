6cd44cb6552b33f845a9c88fc247c0a94c6d6abc
Ashish <ashishrao123@gmail.com>
2021-10-30 09:08:47 +0530

    stage_01_load_save added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,8 +1,16 @@
 import yaml
 import os
+import json
 
-def read_yaml(path_ot_yaml:str)-> dict:
-    with open(path_ot_yaml) as yaml_file:
-        content=yaml.safe_load(yaml_file)
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
 
     return content
+
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"directory is created at {dir_path}")
